b6592949e78f10bf56b2fd3997239f0a7fbf4268
qian-yi <qian-yi@qq.com>
2021-03-30 23:06:58 +0800

    static以及变更检查策略

--- src/app/pages/home/components/wy-carousel/wy-carousel.component.ts
+++ src/app/pages/home/components/wy-carousel/wy-carousel.component.ts
@@ -4,13 +4,17 @@ import { Component, OnInit, TemplateRef, ViewChild, Input, Output, EventEmitter,
   selector: 'app-wy-carousel',
   templateUrl: './wy-carousel.component.html',
   styleUrls: ['./wy-carousel.component.less'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush // 更改变更检测策略，OnPush 只有在 当前 Input依赖的值发生变化后才重新检测该页面是否改变
 })
 export class WyCarouselComponent implements OnInit {
   @Input() activeIndex = 0;
 
   @Output() changeSlide = new EventEmitter<'pre' | 'next'>();
 
+  /**
+   * static 在变更检测前解析模板 还是之后  true: 之前
+   * 如果是动态的，则需要设置为 false  如：ng-if
+   */
   @ViewChild('dot', { static: true }) dotRef: TemplateRef<any>;
 
   constructor() { }
